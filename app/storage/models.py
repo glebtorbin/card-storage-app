23ecb90347e6bd3e81adf798f90e68d8795a9099
Gleb torbin <glebb0@admins-MacBook-Pro.local>
2022-12-08 19:28:28 -0800

    first commit

---
+++ app/storage/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+ACT = 'ACTIVE'
+DEACT = 'DEACTIVATED'
+OVD = 'OVERDUE'
+
+CARD_STATUSES = (
+    (ACT, 'Active'),
+    (DEACT, 'Deactivated'),
+    (OVD, 'Overdue')
+)
+
+class Card(models.Model):
+    series = models.PositiveSmallIntegerField('series', default=0)
+    number = models.PositiveSmallIntegerField('number', default=0)
+    release_date = models.DateTimeField('release at', auto_now_add=True)
+    ending_date = models.DateTimeField('terminates at', auto_now_add=False)
+    status = models.CharField('status', max_length=11,
+                  choices=CARD_STATUSES,
+                  default=DEACT)
+
+class Payment(models.Model):
+    card = models.ForeignKey(
+        Card, on_delete=models.CASCADE,
+        related_name='payments'
+    )
+    purchase_amount = models.DecimalField('purchase amout', max_digits=5, decimal_places=2)
+    purchase_date = models.DateTimeField('purchase date', auto_now_add=False)
